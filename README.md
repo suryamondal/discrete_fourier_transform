60a410d7d96e56d9997ca41eff050d6ad5b337ee
Surya <suryamondal@gmail.com>
2022-04-23 21:34:25 +0200

    readme modified. added a few more frequncies

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# Discreate Fourier Transform
+# Discrete Fourier Transform
 
+This is a basic code for fast Fourier transform based on Cooley-Turkey.
+Here, a few a combination of frequency is generated. Then Fourier
+transform is performed on the spectrum.
+
+A few frequencies then removed from the Fourier spectra. The inverse
+Fourier transform is performed on the Fourier spectra.
+
+Compile and run with:
 ```
-Work in progress.
+cd build
+make
+cd ..
+./fft_main
 ```
+
+For more info, one might check this [website](https://fakephysicist.com/misc/cooley-turkey-dfft-vectors-cpp/).
--- src/main.cc
+++ src/main.cc
@@ -9,15 +9,15 @@
 #include "fft.h"
 
 
-const double tPD = 30.;	// Total Path Difference in sec
-const double maxFrequency = 5.; // Maximum Frequency to Calculate
+const double tPD = 20.;		 // Total Path Difference in sec
+const double maxFrequency = 10.; // Maximum Frequency to Calculate
 
-const Long64_t maxGenPt = 5.;	     // If you force
+const Long64_t maxGenPt = 1.;	     // If you force
 const Long64_t maxDataPt = pow(2,1); // If you force
 
 int main() {
 
-  const Long64_t minPt = 2.*(maxFrequency*tPD+1);
+  const Long64_t minPt = 3.*(maxFrequency*tPD+1);
   const Long64_t inputDataPt = maxGenPt*minPt;
 
   std::cout << " Generating Data...." << std::endl;
@@ -26,19 +26,20 @@ int main() {
   pos0.clear(); pos0.reserve(inputDataPt);
   amp0.clear(); amp0.reserve(inputDataPt);
   
-  /****** Generating Data Points ********/
+  /** Generating Data Points */
   for(int ij=0;ij<inputDataPt;ij++) {
     double tt = ij*tPD/inputDataPt;
-    // double amp = 5.*tt		     // Addition of 1000, 1500, 2000,
-    //   + sin(2.*TMath::Pi()*1000.*tt) // 3000 and 4000 Hz Sine Wave
-    //   + sin(2.*TMath::Pi()*1500.*tt)
-    //   + sin(2.*TMath::Pi()*2000.*tt)
-    //   + sin(2.*TMath::Pi()*3000.*tt)
-    //   + sin(2.*TMath::Pi()*4000.*tt);
     double amp =
-      1.*sin(2.*TMath::Pi()*4.9*tt) +
-      3.*sin(2.*TMath::Pi()*5.0*tt) +
-      2.*sin(2.*TMath::Pi()*5.1*tt) ;
+      1.*sin(2.*TMath::Pi()*4.5*tt) +
+      2.*sin(2.*TMath::Pi()*4.6*tt) +
+      3.*sin(2.*TMath::Pi()*4.7*tt) +
+      4.*sin(2.*TMath::Pi()*4.8*tt) +
+      5.*sin(2.*TMath::Pi()*4.9*tt) +
+      6.*sin(2.*TMath::Pi()*5.0*tt) +
+      7.*sin(2.*TMath::Pi()*5.1*tt) +
+      8.*sin(2.*TMath::Pi()*5.2*tt) +
+      9.*sin(2.*TMath::Pi()*5.3*tt) +
+      10.*sin(2.*TMath::Pi()*5.4*tt) ;
     pos0.push_back(tt);
     amp0.push_back(amp);
   }
@@ -64,26 +65,38 @@ int main() {
     data_C.push_back(tempComp);
   }
 
-  /****** d-FFT *********/
+  /** d-FFT */
+
   std::cout << " DFT Starts...." << std::endl;
   fft::dft fft_straight(data_C);
   fft_straight.doDFT();
   fft::dft::CNArray out_data_C = fft_straight.getDFT();
   std::cout << " DFT Ends...." << std::endl;
-  
+
+  double maxHeight = 0;
   std::vector<double> cBin,cAmp;
   cBin.clear(); cBin.reserve(nn/2);
   cAmp.clear(); cAmp.reserve(nn/2);
   for(int ij=0;ij<nn/2;ij++) {
     cBin.push_back(ij/tPD);
-    cAmp.push_back(abs(out_data_C[ij]));
+    double tmpAmp = abs(out_data_C[ij]);
+    cAmp.push_back(tmpAmp);
+    if(maxHeight<tmpAmp) {maxHeight = tmpAmp;}
   }
 
-  /****** Inverse d-FFT *********/
+  /** Inverse d-FFT */
+
   fft::dft::CNArray data_I = fft_straight.getFullDFT();
+
+  /** removing background bellow 50% */
+  for(auto it=data_I.begin(); it!=data_I.end(); it++) {
+    if(abs(*it)<maxHeight*0.5) {
+      fft::dft::Complex tempComp(0.,0.);
+      *it = tempComp; }}
+  
   std::cout << " iDFFT Starts...." << std::endl;
   fft::dft fft_inverse(data_I);
-  fft_straight.doiDFT();
+  fft_inverse.doiDFT();
   fft::dft::CNArray out_data_I = fft_inverse.getFullDFT();
   std::cout << " iDFFT Ends...." << std::endl;
 
@@ -102,21 +115,26 @@ int main() {
   TGraph* h1 = new TGraph(int(nn/2),&cBin[0],&cAmp[0]);	  // d-FFT
   TGraph* h2 = new TGraph(nn,&iBin[0],&iAmp[0]);	  // id-FFT
   
-
   c1->cd(1);
+  h3->SetTitle("Generated");
   h3->Draw("AL*");
+  h3->GetXaxis()->SetRangeUser(0., tPD);
   c1->cd(2);
+  h0->SetTitle("Padded");
   h0->Draw("AL");
+  h0->GetXaxis()->SetRangeUser(0., tPD);
   c1->cd(3);
+  h1->SetTitle("DFT");
   h1->Draw("AL*");
   // gPad->SetLogy(1);
   h1->GetXaxis()->SetRangeUser(4., 6.);
-  // h1->GetXaxis()->SetRangeUser(1995., 2005.);
   c1->cd(4);
+  h2->SetTitle("Inverse transform");
   h2->Draw("AL");
-  
-  c1->SaveAs("fft.jpg");
-  c1->SaveAs("fft.C");
+  h2->GetXaxis()->SetRangeUser(0., tPD);
+
+  c1->SaveAs("fft.pdf");
+  // c1->SaveAs("fft.C");
   
   return 0;
 }
